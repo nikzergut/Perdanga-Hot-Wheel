781021ecce44ff9c3e63d59c8f7e004555e98063
nikzergut <nikzergut@gmail.com>
2025-02-14 13:25:29 +0700

    added navbar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import styles from './Nav.module.css';
+
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="">Main</a>
+        </li>
+        <li>
+          <a href="">Events</a>
+        </li>
+        <li>
+          <a href="">Profile</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
